7b4769495405fa74a713b7ca5579b7c67113a90e
unforkableco <anton@unforkable.co>
2023-02-17 15:10:44 +0100

    updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [ $# -lt 2 ]
-then
-echo "install script"
-echo "arg1: influx token"
-echo "arg2: influx bucket"
-exit 0
-fi
-
 # Get the public IP address of the machine
 nat_address=$(curl ifconfig.me)
 
@@ -23,13 +15,6 @@ binary_path="/usr/local/bin/nxchain"
 
 # Set the path to the data directory
 data_dir="/data/nxchain"
-
-# Set the path to store telegraf secrets
-telegraf_conf_dir="/usr/local/telegraf"
-telegraf_conf_file="telegraf.env"
-
-influx_token=$1
-influx_bucket=$2
 
 # Set any additional flags or options for the node
 flags="--price-limit 5000000000 --nat $nat_address --chain $data_dir/$chain --libp2p 0.0.0.0:4546 --jsonrpc :4545 --seal"
@@ -77,40 +62,4 @@ WantedBy=multi-user.target
 EOL"
 
 sudo systemctl enable nxchain
-
-# Install telegraf
-wget -q https://repos.influxdata.com/influxdata-archive_compat.key
-echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
-echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
-sudo apt-get update
-sudo apt-get install telegraf -y
-
-# Create telegraf service file
-sudo bash -c "cat >/etc/systemd/system/telegraf.service <<EOL
-[Unit]
-Description=Telegraf
-After=network.target
-StartLimitIntervalSec=0
-
-[Service]
-Type=simple
-Restart=always
-RestartSec=10
-ExecStart=telegraf --config https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/telegrafs/0abf0c860d2c4000
-EnvironmentFile=\"/$telegraf_conf_dir/$telegraf_conf_file\"
-User=root
-
-[Install]
-WantedBy=multi-user.target
-EOL"
-
-sudo mkdir $telegraf_conf_dir
-
-sudo bash -c "cat >$telegraf_conf_dir/$telegraf_conf_file <<EOL
-INFLUX_TOKEN=$influx_token
-INFLUX_BUCKET=$influx_bucket
-EOL"
-
-sudo systemctl enable telegraf
-sudo systemctl start telegraf
 sudo systemctl start nxchain
